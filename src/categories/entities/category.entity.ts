a2a13dc3527eb9346279fe09b08b2e859ded1481
afgomezv <afgomezvonline@gmail.com>
2025-02-10 17:26:07 -0500

    module categories

---
+++ src/categories/entities/category.entity.ts
@@ -0,0 +1,10 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 60 })
+  name: string;
+}
