c3e1c4de025b624d85cd8c2f94320f459c161ca6
afgomezv <afgomezvonline@gmail.com>
2025-02-11 11:06:24 -0500

    module products

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,22 @@
+import { IsInt, IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
+
+export class CreateProductDto {
+  @IsNotEmpty({ message: 'El nombre del producto es obligatorio' })
+  @IsString({ message: 'El nombre del producto debe ser un texto' })
+  name: string;
+
+  @IsNotEmpty({ message: 'El precio es obligatorio' })
+  @IsNumber({ maxDecimalPlaces: 2 }, { message: 'El precio no es válido' })
+  @Min(0, { message: 'El precio no puede ser negativo' })
+  price: number;
+
+  @IsNotEmpty({ message: 'La cantidad es obligatoria' })
+  @IsNumber({ maxDecimalPlaces: 0 }, { message: 'La cantidad no es válida' })
+  @Min(0, { message: 'La cantidad no puede ser negativa' })
+  quantity: number;
+
+  @IsNotEmpty({ message: 'La categoría es obligatoria' })
+  @IsInt({ message: 'La categoría no es un número' })
+  @Min(1, { message: 'La categoría no puede ser menor a 1' })
+  categoryId: number;
+}
